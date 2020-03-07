e74c11e20d3295e295f3c4101533ed64156aed6c
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-07 11:13:09 -0500

    finial without comments

--- main.ts
+++ main.ts
@@ -7,6 +7,32 @@ radio.onReceivedValue(function (name, value) {
         pins.servoWritePin(AnalogPin.P4, value)
     }
 })
+function clearBall () {
+    pins.servoWritePin(AnalogPin.P0, 30)
+    pins.servoWritePin(AnalogPin.P2, 30)
+    pins.servoWritePin(AnalogPin.P4, 30)
+}
+function reset () {
+    radio.setGroup(70)
+    pins.servoWritePin(AnalogPin.P0, 0)
+    pins.servoWritePin(AnalogPin.P2, 0)
+    pins.servoWritePin(AnalogPin.P4, 0)
+}
+function dispenseBall () {
+    radio.setGroup(70)
+    pins.servoWritePin(AnalogPin.P10, 45)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P10, 100)
+}
+input.onButtonPressed(Button.AB, function () {
+    clearBall()
+})
+input.onButtonPressed(Button.B, function () {
+    dispenseBall()
+})
+input.onButtonPressed(Button.A, function () {
+    reset()
+})
 pins.analogSetPeriod(AnalogPin.P0, 20000)
 pins.servoSetPulse(AnalogPin.P0, 2000)
 pins.servoWritePin(AnalogPin.P0, 0)
@@ -16,3 +42,5 @@ pins.servoWritePin(AnalogPin.P2, 0)
 pins.analogSetPeriod(AnalogPin.P4, 20000)
 pins.servoSetPulse(AnalogPin.P4, 2000)
 pins.servoWritePin(AnalogPin.P4, 0)
+pins.analogSetPeriod(AnalogPin.P10, 20000)
+pins.servoSetPulse(AnalogPin.P10, 2000)
